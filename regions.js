8e72f726ecb989cd6fc1ee3b3df68944773563f0
DanielKulkamp <daniel.kulkamp@gmail.com>
2021-06-03 01:58:48 -0300

    drawing circle

--- regions.js
+++ regions.js
@@ -1,1 +1,12 @@
-//orc
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM completamente carregado e analisado");
+    let canvas = document.getElementById("cv")
+    ctx = canvas.getContext("2d")
+    ctx.fillStyle = "rgb(200, 0, 0)"
+    ctx.strokeStyle = "rgb(0,0,200)"
+    ctx.moveTo(100,0)
+    ctx.beginPath()
+    ctx.arc(100,100, 100, 100 , 2*Math.PI, true)
+    ctx.closePath()
+    ctx.stroke()
+})
